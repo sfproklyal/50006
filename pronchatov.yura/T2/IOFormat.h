ae19425ba57fde1ceba498232106eda179751e81
pronchatov.yura <pronchatovyura@gmail.com>
2026-06-02 11:47:28 +0300

    new commit for T2

---
+++ pronchatov.yura/T2/IOFormat.h
@@ -0,0 +1,48 @@
+#ifndef IOFORMAT_H
+#define IOFORMAT_H
+
+#include <ios>
+#include <string>
+
+struct DelimeterIO
+{
+    char exp;
+};
+
+struct DblSciIO
+{
+    double& ref;
+};
+
+struct UllBinIO
+{
+    unsigned long long& ref;
+};
+
+struct StringIO
+{
+    std::string& ref;
+};
+
+std::istream& operator>>(std::istream& in, DelimeterIO&& dest);
+std::istream& operator>>(std::istream& in, DblSciIO&& dest);
+std::istream& operator>>(std::istream& in, UllBinIO&& dest);
+std::istream& operator>>(std::istream& in, StringIO&& dest);
+
+std::string formatDblSci(double value);
+std::string formatUllBin(unsigned long long value);
+
+class iofguard
+{
+public:
+    explicit iofguard(std::basic_ios<char>& s);
+    ~iofguard();
+
+private:
+    std::basic_ios<char>& s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags fmt_;
+};
+
+#endif
---
+++ pronchatov.yura/T2/main.cpp
@@ -0,0 +1,38 @@
+#include "DataStruct.h"
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <vector>
+
+
+int main()
+{
+    std::vector<DataStruct> data;
+
+    while (!std::cin.eof())
+    {
+        std::copy(
+            std::istream_iterator<DataStruct>(std::cin),
+            std::istream_iterator<DataStruct>(),
+            std::back_inserter(data)
+        );
+
+        if (!std::cin.eof() && std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+
+    std::sort(data.begin(), data.end(), compareDataStruct);
+
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+
+    return 0;
+}
